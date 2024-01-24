039d6efc92dce8697b02d54ddb8330d98a3c820b
SuryaB1 <surya.j.bala@gmail.com>
2024-01-24 16:34:14 -0500

    Complete main function and VoronoiCell class functionality with exception of ridge computations for corner centroids in input space

--- nn_piecewise_cegis_voronoi_binary-class.py
+++ nn_piecewise_cegis_voronoi_binary-class.py
@@ -24,64 +24,96 @@ DEBUG = True
 SYNTHESIS_LOWER_BOUND = -100
 SYNTHESIS_UPPER_BOUND = 100
 
+MIN_COUNTEREX_DISTANCE_TO_RIDGE = 1 # minimum distance a counterexample can be from a segment boundary to be considered
+
 # Counterexample handling states
 SET_STATE = 0
 SPLIT_STATE = 1
 
 @dataclass
 class Hyperplane:
-    coeffs: list # list of floats
+    coeffs: np.ndarray
     constant: float
     type: MarabouCore.Equation.EquationType
 
-class VoronoiCell:
-    input_dim = 2
+    def distance_to_hyperplane(self, point):
+        unscaled_distance = np.dot(self.coeffs, point) + self.constant
+        return unscaled_distance / np.linalg.norm(self.coeffs)
 
-    def __init__(self, centroid, ridges, output, neighbors=[]):
+class VoronoiCell:
+    vor = None
+    def __init__(self, centroid, ridges, output, neighbors):
         self.centroid = centroid
         self.ridges = ridges
         self.output = output
         self.neighbors = neighbors
+
+    def compute_dist_to_closest_ridge(self, point):
+        return min([ridge.distance_to_hyperplane(point) for ridge in self.ridges])
     
     @staticmethod
-    def compute_voronoi_tessellation(input_output_dict, output_var_idx):
+    def compute_voronoi_tessellation(input_output_dict, add_points=False):
         # Obtain Voronoi tessellation
         new_centroids = list(input_output_dict.keys())
-        vor = Voronoi(new_centroids)
-        bounded_ridges = [ridge for ridge in vor.ridges if not -1 in ridge]
+        if add_points:
+            vor.add_points(new_centroids)
+        else:
+            vor = Voronoi(new_centroids, incremental=True)
 
         # Construct new cells from generated centroids and Voronoi tessellation
         new_cells = []
         for i in range(len(vor.points)):
             centr = vor.points[i]
-            ridges = VoronoiCell.get_ridges_for_region(centr, vor.regions[i], bounded_ridges, vor.vertices)
-            new_cells.append(VoronoiCell(centr, ridges, input_output_dict[centr]))
-
-        # TODO: populate .neighbors with O(n^2) for loop comparing ridges and search for match
+            ridges, neighbors = VoronoiCell.get_ridges_and_neighbors_for_region(centr, i, vor.ridge_dict, vor.vertices)
+            new_cells.append(VoronoiCell(centr, ridges, input_output_dict[centr], neighbors))
 
         return new_cells
     
     @staticmethod
-    def get_ridges_for_region(centroid, region, bounded_ridges, vertices): # Given a region and a set of bounded regions, return the ridges of this region as Hyperplane objects
-        region = set(region)
-        ridges_of_given_region = []
-        for ridge in bounded_ridges:
-            if set(ridge).issubset(region):
-                ridges_of_given_region.append(VoronoiCell.get_hyperplane(centroid, ridge, vertices))
-        return ridges_of_given_region
+    def get_ridges_and_neighbors_for_region(centroid, region_idx, ridge_dict, vertices): # Given a region and a list of all ridges, return the ridges of this region as Hyperplane objects
+        neighbors = []
+        ridges = []
+        for adj_region_pair, ridge in ridge_dict.items():
+            # if ridge[0] == -1:
+            #     ridge = ridge[1:]
+
+            # TODO: adapt for corner vertices (from open source):
+            # midpoint = vor.points[pointidx].mean(axis=0)
+            # direction = np.sign(np.dot(midpoint - center, n)) * n
+            # if (vor.furthest_site):
+            #     direction = -direction
+            # far_point = vor.vertices[i] + direction * ptp_bound.max()
+
+            # infinite_segments.append([vor.vertices[i], far_point])
+                
+            if adj_region_pair[0] == region_idx: #TODO: include corner vertices (only one point for region)
+                ridges.append( VoronoiCell.get_hyperplane(centroid, ridge, vertices) )
+                neighbors.append(adj_region_pair[1])
+            elif adj_region_pair[1] == region_idx:
+                ridges.append( VoronoiCell.get_hyperplane(centroid, ridge, vertices) )
+                neighbors.append(adj_region_pair[0])
+
+        return ridges, neighbors
 
     @staticmethod
     def get_hyperplane(centroid, ridge, vertices): # Given a ridge, return it as a hyperplane by solving Ax=b (where x is the coefficients) using least squares
-        A = np.array(vertices[ridge]) # Array of vertices
-        b = np.ones(A.shape[0])
-
-        coefficients = np.linalg.pinv(A) @ b # TODO: compare with np.linalg.lstsq for computing hyperplane from vertices
-        constant = 1
+        # Arbitrary-dimension case
+        # A = np.array(vertices[ridge]) # Array of vertices
+        # b = np.ones(A.shape[0])
+        # coefficients = np.linalg.pinv(A) @ b # TODO: compare with np.linalg.lstsq for computing hyperplane from vertices
+        # constant = 1
         
-        inequality_type = None
-        if centroid dot coeffs < constant: 
+        # 2D case
+        y_delta = vertices[ridge][1][1] - vertices[ridge][0][1]
+        x_delta = vertices[ridge][1][0] - vertices[ridge][0][0]
+        x_coeff = y_delta / x_delta if x_delta != 0 else -1
+        y_coeff = int(x_delta != 0)
+        constant = y_coeff * vertices[ridge][0][1] - x_coeff * vertices[ridge][0][0]
+        coeffs = np.asarray([-1*x_coeff, y_coeff])
+
+        if np.dot(coeffs, centroid) < constant: 
             inequality_type = MarabouCore.Equation.EquationType.LE
-        elif centroid dot coeffs < constant:
+        elif np.dot(coeffs, centroid) > constant:
             inequality_type = MarabouCore.Equation.EquationType.GE
         else:
             raise ValueError("Centroid lies on VoronoiCell ridge")
@@ -150,20 +182,24 @@ def main():
     VoronoiCell.input_dim = len(input_vars)
     init_datapoints = read_init_datapoints("diagonal-split_classif_nnet_sample_datapoints.txt", len(input_vars), len(output_vars))
     
-    counterexample_handling_state = SET_STATE
     # Loop 1: Synthesize piecewise mapping for each output variable (this algorithm assumes each output is an integer)
     for i in tqdm(range(len(output_vars))):
         debug_log(f"--- Searching piecewise mapping for Output Variable {i} ---")
         output_var = output_vars[i]
 
-        centroid_cell_map = dict() # Dictionary of all searched centroids in input space mapped to corresponding VoronoiCell objects
+        centroid_cell_map = dict() # Dictionary of all centroids in input space mapped to corresponding VoronoiCell objects
         incomplete_segments = [] # Stack of centroids of segments left to search for counterexamples
         
         if init_datapoints:
-            incomplete_segments.extend(VoronoiCell.compute_voronoi_tessellation(init_datapoints, i))
+            new_cells = VoronoiCell.compute_voronoi_tessellation(init_datapoints, i)
+            for cell in new_cells:
+                incomplete_segments.append(cell.centroid)
+                centroid_cell_map[cell.centroid] = cell
         else: # if no user-given initial datapoint, set input space as the initial Voronoi cell
             init_centroid = tuple([ (SYNTHESIS_LOWER_BOUND + SYNTHESIS_UPPER_BOUND) / 2 for i in range(len(input_vars)) ])
-            initial_cell = VoronoiCell(init_centroid, construct_initial_ridge_vertices(input_vars), 0)
+            network.clearProperty()
+            init_output = network.evaluateWithMarabou(init_centroid)
+            initial_cell = VoronoiCell(init_centroid, construct_initial_ridge_vertices(input_vars), int(init_output), [])
 
             centroid_cell_map[init_centroid] = initial_cell
             incomplete_segments.append(initial_cell)
@@ -171,18 +207,24 @@ def main():
         while incomplete_segments:
             curr_segment = incomplete_segments.pop()
 
-            network.clearProperty()
-            form_query(network, output_var, centroid_cell_map[curr_segment]) # Use current segment inequalities on Marabou
-            exitCode, vals, stats = network.solve(verbose=False) # Query Marabou for counterexamples within segment
+            for counterex_handling_state in range(2):
+                network.clearProperty()
+                form_query(network, output_var, centroid_cell_map[curr_segment]) # Use current segment inequalities on Marabou
+                exitCode, vals, stats = network.solve(verbose=False) # Query Marabou for counterexamples within segment
+                
+                counterex_centroid = tuple([float(vals[var]) for var in input_vars])
+                if len(vals) and centroid_cell_map[curr_segment].compute_dist_to_closest_ridge(counterex_centroid) > MIN_COUNTEREX_DISTANCE_TO_RIDGE:
+                    if counterex_handling_state == SET_STATE:
+                        centroid_cell_map[curr_segment].output = int(vals[output_var])
+                    elif counterex_handling_state == SPLIT_STATE:
+                        centroid_cell_map[curr_segment].output = int(not int(vals[output_var]))
 
-            if len(vals):
-                if counterexample_handling_state == SET_STATE:
-                    centroid_cell_map[curr_segment].output = int(vals[output_var])
-                elif counterexample_handling_state == SPLIT_STATE:
-                    new_centroid = tuple([float(vals[var]) for var in input_vars])
-                    # centroid_cell_map.keys() + new_centroid for regenerating voronoi tessellation
-                    incomplete_segments.append(new_centroid)
-                    # TODO: see line 20 (need to merge Voronoi tessellation of new centroid with non-uniform boundary of existing segment)
+                        incomplete_segments.append(counterex_centroid)
+                        updated_cells = VoronoiCell.compute_voronoi_tessellation({counterex_centroid:centroid_cell_map[curr_segment].output}, add_points=False)
+                        centroid_cell_map[counterex_centroid] = updated_cells[0]
+                else:
+                    break
+
 
 
 if __name__ == '__main__':
